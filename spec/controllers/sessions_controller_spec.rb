d419e708333386e7ef08b3b78b4cec4bfb96ab99
Andrew Ham <hampossible@gmail.com>
2015-06-23 11:56:12 -0700

    added spec test files

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+	describe "sessions controller" do
+		it "tests the sessions controller" do 
+			assert true
+		end
+	end
+ end
